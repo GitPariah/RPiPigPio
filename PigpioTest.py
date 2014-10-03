1e03a54fe03fc4d9c3970ebad8c98aadad0d5feb
GitPariah <x25nova@gmail.com>
2014-10-04 00:29:31 +1000

    For one motor
    For now instead of testing and setting up all of them try just one im not sure if this directly works because my pi is doing something atm, you could comment what your problem is on your code or errors

--- PigpioTest.py
+++ PigpioTest.py
@@ -1,36 +1,35 @@
 import pigpio
 from time import sleep
+#For now only test one motor on GPIO7 (Pin26) http://imagizer.imageshack.us/a/img577/3040/raspberrypirev2pinout.jpg
+#Motor1A = 16
+#Motor1B = 18
+Motor1E = 7
 
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+#Define PWM here or use as a refrence
+PWM = 128
+Rate_of_change = 32
+Direction = 1
 
-Motor2A = 19
-Motor2B = 21
-Motor2E = 23
+
+#Motor2A = 19
+#Motor2B = 21
+#Motor2E = 23
+
 pi = pigpio.pi()
-
-pi.set_mode(Motor1A, pigpio.OUTPUT)
-pi.set_mode(Motor1B, pigpio.OUTPUT)
+#pi.set_mode(Motor1A, pigpio.OUTPUT)
+#pi.set_mode(Motor1B, pigpio.OUTPUT)
 pi.set_mode(Motor1E, pigpio.OUTPUT)
-pi.set_PWM_range(9,100)
+#pi.set_PWM_range(9,100)
 
-pi.write(Motor1A, 0)
-pi.write(Motor1B, 1)
-pi.write(Motor1E, 1)
+for x in range(0, 5): #For five iterations http://www.raspberrypi.org/forums/viewtopic.php?f=33&t=88323
+  
+  gpioPWM(Motor1E, PWM) #Read up http://abyz.co.uk/rpi/pigpio/cif.html#gpioPWM
+  PWM += (Direction * Rate_of_change)
+  if PWM < 0 or PWM > 255:
+    Direction = -Direction
+  time.sleep(1)
+#pi.write(Motor1A, 0)
+#pi.write(Motor1B, 1)
+#pi.write(Motor1E, 1)
 
-pi.set_PWM_dutycycle(Motor1E, 0)
-sleep(1)
-print("Duty cycle 25")
-pi.set_PWM_dutycycle(Motor1E, 25)
-sleep(1)
-print("Duty cycle 40")
-pi.set_PWM_dutycycle(Motor1E, 40)
-sleep(1)
-print("Duty cycle 80")
-pi.set_PWM_dutycycle(Motor1E, 80)
-sleep(1)
-print("Duty cycle 100")
-pi.set_PWM_dutycycle(Motor1E, 100)
-sleep(1)
 pi.stop()
